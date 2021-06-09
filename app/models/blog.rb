bf76ef15e9903ce3260cafec996a9bca6464bfd4
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-09 06:19:06 +0000

    init

---
+++ app/models/blog.rb
@@ -0,0 +1,5 @@
+class Blog < ApplicationRecord
+  validates :content, presence: true, length: { in: 1..140 }
+  validates :content, length: { maximum: 140 }
+  validates :content, presence: true
+end
